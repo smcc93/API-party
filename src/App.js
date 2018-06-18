7eb1faeb0d88f1e6e2867ac4711e9a77cb62b42e
Stewart McCalley <smccalle@iu.edu>
2018-06-18 14:41:39 -0400

    add github component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Route, Switch, NavLink} from 'react-router-dom'
 
+import Github from './Github'
 import './App.css';
 
 class App extends Component {
@@ -17,7 +18,7 @@ class App extends Component {
          </ul>
 
         <Switch>
-          <Route path="/github" render={()=> <h1>GitHub!</h1>} />
+          <Route path="/github" component={Github} />
           <Route render={()=> <p>To get started, click one of the links above.</p>}/>
          </Switch> 
       </div>
---
+++ src/Github.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Github extends Component {
+    render(){
+        return(
+            <div className="Github">
+            testing
+            </div>
+        )
+    }
+}
+export default Github
